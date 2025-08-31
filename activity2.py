b30b9e2c71ec3e931e30d348caaccb8818b11ae3
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-08-31 01:36:40 -0700

    1

---
+++ activity2.py
@@ -0,0 +1,17 @@
+# TYPECASTING : FROM STRING TO INTEGER (NUMBER)
+
+a='200'
+print(a)
+print(type(a))
+b=int(a)
+print(b)
+print(type(b))
+
+#TYPECASTING FROM STRING TO INTEGER (ALPHABET)
+
+a='r'
+print(a)
+print(type(a))
+b=int(a)
+print(b)
+print(type(b))
